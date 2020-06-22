116a8b5c5cca09be909434e4a814e6433689fd70
shadmanhiya <shadmanhiya09@gmail.com>
2020-06-22 19:13:10 +0600

    First Commit String

---
+++ string.py
@@ -0,0 +1,8 @@
+"""
+Write code using find() and string slicing to extract the number at the end of the line below. Convert the extracted value to a floating
+point number and print it out.
+"""
+text = "X-DSPAM-Confidence:    0.8475";
+number = text.find('0')
+slic = text[number : len(text)]
+print(float(slic))
